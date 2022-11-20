2a98d68db9a97019bdc8f821d390f01fdd59da26
panithee <panithee08@gmail.com>
2022-11-20 21:44:47 +0700

    template

---
+++ src/scenes/GameScene.js
@@ -0,0 +1,25 @@
+import Phaser from "phaser";
+
+
+
+class GameScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'GameScene'
+        });
+    }
+
+    preload() {
+        // code here
+
+    }
+
+    create() {
+        
+    }
+
+    update(delta, time) {
+        
+    }
+}
+export default GameScene;
