51119e7a28404ab574c0fe10d0e56c1c046f15aa
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2022-11-24 20:34:56 +0700

    scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,7 +11,7 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('bg','/src/GameScene/4.png');
-        this.load.image('platform','src/GameScene/7.png');
+        this.load.image('platform','src/GameScene/grassfloor.png');
 
     }
 
