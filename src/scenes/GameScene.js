0ad85f1c36050564ce2793e0ac738b858244b42f
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2022-11-24 20:29:22 +0700

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 let background;
 let slime;
+let platforms;
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -10,26 +11,19 @@ class GameScene extends Phaser.Scene {
 
     preload() {
         this.load.image('bg','/src/GameScene/4.png');
-        //bg.fixedToCamera = true;
-        this.load.spritesheet('slime','/src/GameScene/Slime.png',{frameWidth: 315, frameHeight: 312});
+        this.load.image('platform','src/GameScene/7.png');
+
     }
 
     create() {
         background = this.add.image(800,500,'bg');
-        slime = this.physics.add.sprite(600,700,'slime').setScale(0.6);
-        this.anims.create({
-            key: 'slime1',
-            frame: this.anims.generateFrameNumbers('slime',{
-                start:0,
-                end:1
-            }),
-            duration:500,
-            repeat:-1
-        })
+        platforms = this.physics.add.staticGroup();
+        platforms.create(800, 980, 'platform').refreshBody();
+
     }
 
     update(delta, time) {
-        slime.anims.play('slime1',true);
+        
     }
 }
 export default GameScene;
