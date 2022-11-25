e3eee9a573b49d85c177b8a8bb069b5ac9eb3b64
Pumrapee <110712445+Pumrapee@users.noreply.github.com>
2022-11-25 22:45:18 +0700

    Update MainMenu.js

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -13,11 +13,11 @@ class MainMenu extends Phaser.Scene {
     preload() {
         this.load.image('startButton','src/GameScene/playButton.png')
         this.load.image('slimelogo','src/GameScene/Slimeking_logo.png')
-        this.load.image('map','src/GameScene/map.png')
+        this.load.image('bg','src/GameScene/4.png')
     }
 
     create() {
-        background = this.add.image(5760,540,'map')
+        background = this.add.image(960,540,'bg')
         playButton = this.add.image(950,720,'startButton').setScale(0.5)
         this.add.image(950,360,'slimelogo').setScale(2)
         playButton.setInteractive()
