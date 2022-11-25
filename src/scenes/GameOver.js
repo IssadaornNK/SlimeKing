e6ed96a1e71450d399973d5174e40762f61a5607
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2022-11-26 00:24:22 +0700

    add

---
+++ src/scenes/GameOver.js
@@ -0,0 +1,28 @@
+import Phaser from "phaser";
+
+class GameOver extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: "GameOver",
+        });
+    }
+
+    preload() {
+        this.load.image('bg','src/GameScene/GameOver.png');
+        this.laod.image('restart','src/GameScene/pngkey.com-replay-png-2243342.png');
+    }
+
+    create() {
+        background = this.add.image(960,540,'bg')
+        playButton = this.add.image(950,720,'replay').setScale(0.5)
+        //this.add.image(950,360,'slimelogo').setScale(2)
+        playButton.setInteractive()
+        playButton.on("pointerdown",()=>{
+            this.scene.start("GameScene")
+        })
+    }
+
+    update(delta, time) {
+    }
+}
+export default GameOver;
