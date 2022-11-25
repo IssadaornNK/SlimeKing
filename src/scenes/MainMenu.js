595c07d61913e7edb67351405629e298ef2f8c44
IssadaornNK <111173930+Issadaorn@users.noreply.github.com>
2022-11-26 05:09:03 +0700

    dfdf

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -2,7 +2,7 @@ import Phaser from "phaser";
 
 let playButton;
 let background;
-
+let sound1;
 class MainMenu extends Phaser.Scene {
     constructor(test) {
         super({
@@ -14,10 +14,12 @@ class MainMenu extends Phaser.Scene {
         this.load.image('startButton','src/GameScene/playButton.png')
         this.load.image('slimelogo','src/GameScene/Slimeking_logo.png')
         this.load.image('bg','src/GameScene/Mainpage.png')
+        this.load.audio('sound1','src/GameScene/taratata-6264.mp3');
     }
 
     create() {
         background = this.add.image(960,540,'bg')
+        sound1 = this.sound.add('sound1', { loop: true })
         playButton = this.add.image(950,420,'startButton').setScale(0.5)
         //this.add.image(950,360,'slimelogo').setScale(2)
         playButton.setInteractive()
@@ -27,6 +29,7 @@ class MainMenu extends Phaser.Scene {
     }
 
     update(delta, time) {
+        sound1.play();
     }
 }
 export default MainMenu;
